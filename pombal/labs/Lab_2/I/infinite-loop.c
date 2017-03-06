9640da4de3b13ec1b0467052202d2ad455b1aa53
FranciscoPombal <pombal.francisco@gmail.com>
2017-03-06 15:20:37 +0000

    First exercise

--- pombal/labs/Lab_2/I/infinite-loop.c
+++ pombal/labs/Lab_2/I/infinite-loop.c
@@ -6,9 +6,17 @@ int main(){
 	int v[100];
 	int i;
 
+    // Why does the program work with a while loop and not a for loop?
+    while(i<100){
+        v[i] = random();
+        i++;
+    }
+
+    /*
 	for (i=0; i<100; i+1){
 		v[i] = random();
-	}
+    }
+    */
 
 	printf("vector initialized\n");
 
