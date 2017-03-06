ad6a6b95d5eadb5a988a6656800f7ac747179828
FranciscoPombal <pombal.francisco@gmail.com>
2017-03-06 16:07:32 +0000

    Exercises 3 and 4

--- pombal/labs/Lab_2/III/char-conv-prob.c
+++ pombal/labs/Lab_2/III/char-conv-prob.c
@@ -8,16 +8,18 @@ int main(){
 	char v1[100];
 	char *v2;
 	int i;
-	
+
 	printf("Write a word");
 	fgets(v1, 100, stdin);
 
-	v2 = malloc(sizeof(char)*strlen(v1));
+	v2 = malloc(sizeof(char)*strlen(v1)+1);
 	for (i=0; v1[i]!=0; i++){
 		v2[i] = toupper(v1[i]);
 	}
 
-	printf("Converted string: %s", v2);
+	printf("Converted string: %s\n", v2);
 
+    free(v2);
 
+    exit(EXIT_SUCCESS);
 }
--- pombal/labs/Lab_2/IV/char-conv-uninit.c
+++ pombal/labs/Lab_2/IV/char-conv-uninit.c
@@ -8,7 +8,7 @@ int main(){
 	char v1[100];
 	char *v2;
 	int i;
-	
+
 	printf("Write a word");
 	fgets(v1, 100, stdin);
 
@@ -16,8 +16,10 @@ int main(){
 	for (i=0; v1[i]!=0; i++){
 		v2[i] = toupper(v1[i]);
 	}
+    v2[i] = '\0';
 
 	printf("Converted string: %s", v2);
 	free(v2);
 
+    exit(EXIT_SUCCESS);
 }
