75aa381824d812ef78a37e764f71d92b3cce44d2
FranciscoPombal <pombal.francisco@gmail.com>
2017-03-06 15:35:28 +0000

    Exercise 2

--- pombal/labs/Lab_2/II/char-conv.c
+++ pombal/labs/Lab_2/II/char-conv.c
@@ -2,20 +2,21 @@
 #include <stdlib.h>
 #include <ctype.h>
 
+#define BUFFER_SIZE 100
 
 int main(){
-	char v1[100];
-	char *v2;
+	char v1[BUFFER_SIZE];
+	char *v2 = (char*)malloc(BUFFER_SIZE*sizeof(char));
 	int i;
-	
-	printf("Write a word");
-	fgets(v1, 100, stdin);
 
-	for (i=0; v1[i]!=0; i+1){
+	printf("Write a word");
+	fgets(v1, BUFFER_SIZE, stdin);
+
+	for (i=0; v1[i]!='\0'; i++){
 		v2[i] = toupper(v1[i]);
 	}
 
 	printf("Converted string: %s", v2);
 
-
+    exit(EXIT_SUCCESS);
 }
