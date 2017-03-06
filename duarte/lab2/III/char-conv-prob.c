69f95e17757c31ebe886cdda8c7a827cf75bdc93
Duarte Dias <DuartexDias@gmail.com>
2017-03-06 14:25:06 +0000

    enunciado

---
+++ duarte/lab2/III/char-conv-prob.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+
+int main(){
+	char v1[100];
+	char *v2;
+	int i;
+	
+	printf("Write a word");
+	fgets(v1, 100, stdin);
+
+	v2 = malloc(sizeof(char)*strlen(v1));
+	for (i=0; v1[i]!=0; i++){
+		v2[i] = toupper(v1[i]);
+	}
+
+	printf("Converted string: %s", v2);
+
+
+}
