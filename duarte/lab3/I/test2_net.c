37a7e0c1dc79f70d5d57944bfdc272d7778abe01
Duarte Dias <DuartexDias@gmail.com>
2017-03-12 01:01:40 +0000

    I. working perfectly

--- duarte/lab3/I/test2_net.c
+++ duarte/lab3/I/test2_net.c
@@ -1,10 +1,11 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include <unistd.h>
 
 int main ( int argc, char *argv[] )
 {
     int i, pid, sleepTime = 0;
+    printf("Parent ID: %d\n\n", getpid());
 
 for(i = 0; i < atoi(argv[1]); i++) {
     pid = fork();
@@ -13,14 +14,16 @@ for(i = 0; i < atoi(argv[1]); i++) {
         exit(1);
     } else if (pid == 0) {
         printf("Child (%d): %d\n", i + 1, getpid());
-        sleepTime = random()%11;
+        srandom(time(NULL));
+        sleepTime = (int) (random()%11);
         sleep(sleepTime);
         wait();
-        printf("%d slept for %d seconds\n", getpid(), &sleepTime);
+        printf("%d slept for %d seconds\n", getpid(), sleepTime);
         exit(0);
     } else  {
         wait(NULL);
     }
 }
+exit(0);
 
 }
