9500b0db9b42b9b7906f1d6e5f1a0df3a3e5db14
FranciscoPombal <pombal.francisco@gmail.com>
2017-03-19 21:58:44 +0000

    Added missing source file content

---
+++ pombal/labs/Lab_4/lab4-1.c
@@ -0,0 +1,31 @@
+#include <limits.h>
+#include <stdio.h>
+
+typedef struct{
+    unsigned int start, end;
+} com_struct;
+
+int main(void){
+    unsigned int m_7, m_19, m_7_19;
+    m_7 = m_19 = m_7_19 = 0;
+    com_struct limits;
+
+        limits.start = 0;
+        limits.end = UINT_MAX;
+        for(unsigned int i = limits.start; i < limits.end; i++){
+            if(i % 7 == 0){
+                m_7++;
+            }
+            if(i % 19 == 0){
+                m_19++;
+            }
+            if((i % 7 == 0) && (i % 19 == 0)){
+                m_7_19++;
+            }
+        }
+        fprintf(stdout,"m 7    %d\n", m_7);
+        fprintf(stdout,"m   19 %d\n", m_19);
+        fprintf(stdout,"m 7 19 %d\n", m_7_19);
+
+    return 0;
+}
