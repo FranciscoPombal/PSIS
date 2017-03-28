b002357b5ed254dac909809794daaf7d88c9c5d2
FranciscoPombal <pombal.francisco@gmail.com>
2017-03-28 13:09:48 +0100

    Initial commit for III

--- pombal/labs/Lab_5/III/III.c
+++ pombal/labs/Lab_5/III/III.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <unistd.h>
+#include <fcntl.h>
 
 int verify_prime(int);
 
@@ -20,6 +22,13 @@ int verify_prime(int n){
 int main(int argc, char const *argv[])
 {
     int n_child = 0;
+    int i = 0;
+    int n = 0;
+    pid_t pid = 0;
+    int pipefd[2];
+    int pipefd2[2];
+    unsigned long long int partial_count = 0;
+    unsigned long long int sum = 0;
 
         if(argc == 1){
             n_child = 0;
@@ -32,7 +41,6 @@ int main(int argc, char const *argv[])
         fprintf(stdout, "number of children %d\n", n_child);
 
         if(n_child == 0){
-            int i, n;
             for(i = 2; i < 99999; i++){
                 n = random() % 999999;
                 if(verify_prime(n)){
@@ -41,9 +49,44 @@ int main(int argc, char const *argv[])
             }
         }else{
             //create n_clid processes
+            pipe(pipefd);
+            pipe(pipefd2);
+            for(i = 0; i < n_child; i++){
+                pid = fork();
+                if(pid < 0){
+                    fprintf(stderr, "fork error.\n");
+                    exit(EXIT_FAILURE);
+                }else if(pid == 0){
+                    //child
+                    close(pipefd2[0]);
+                    while(read(pipefd[0], &i, sizeof(int)) > 0){
+                        if(verify_prime(i)){
+                            fprintf(stdout, "%d is prime\n", i);
+                            partial_count++;
+                        }
+                    }
+                    write(pipefd2[1], &partial_count, sizeof(unsigned long long int));
+                    close(pipefd2[1]);
+                    exit(EXIT_SUCCESS);
+                }
+            }
 
-            //generate random numbers
-            //send those numbers to the child processes
+            for(i = 2; i < 999999; i++) {
+                n = random() % 999999;
+                write(pipefd[1], &n, sizeof(int));
+            }
+            close(pipefd[0]);
+            close(pipefd[1]);
+            while(read(pipefd2[0], &partial_count, sizeof(unsigned long long int)) > 0){
+                sum += partial_count;
+            }
+
+            fprintf(stdout, "Total number of primes: %llu\n", sum);
+
+            close(pipefd2[0]);
+            close(pipefd2[1]);
+
+            wait(NULL);
 
         }
 
