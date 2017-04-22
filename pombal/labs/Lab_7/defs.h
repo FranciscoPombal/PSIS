7f2a424497167612c0c5bd429559a382b297a880
FranciscoPombal <pombal.francisco@gmail.com>
2017-04-22 23:37:00 +0100

    Update linked-list files with some documentation for doxygen (WIP)

--- pombal/labs/Lab_7/defs.h
+++ pombal/labs/Lab_7/defs.h
@@ -1,12 +1,39 @@
+/**
+ * @file defs.h
+ * @author Francisco Pombal
+ * @date 22 April 2017
+ * @brief File containing simple definitions.
+ *
+ * This header file contains:
+ * - definition of a generic pointer type "Item"
+ * - a portable "boolean" type definition
+ * This code is heavily inspired on material obtained from the Algorithms and Data Structures
+ * course at IST Lisboa.
+ * @author Francisco Pombal
+ * @bug No known bugs
+ * @see https://www.tecnico.ulisboa.pt
+ */
+
+
 #ifndef DEFS_H
 #define DEFS_H
 
+/**
+ * @brief Generic pointer type Item
+ *
+ * Saves the pain of always writing the *, making the code easier to read
+ */
 typedef void* Item;
 
+/**
+ * @brief Portable boolean type implementation
+ *
+ * This is useful for some systems that may not have the system stdbool.h header.
+ */
 typedef enum
 {
-    FALSE = 0,
-    TRUE = 1
+    TRUE = 1,   /**< The boolean value "true" */
+    FALSE = 0   /**< The boolean value "false" */
 } Boolean;
 
 #endif
--- pombal/labs/Lab_7/linked_list.h
+++ pombal/labs/Lab_7/linked_list.h
@@ -1,16 +1,124 @@
+/**
+ * @file linked_list.h
+ * @author Francisco Pombal
+ * @date 22 April 2017
+ * @brief File containing function prototypes for a singly/doubly-linked list implementation.
+ *
+ * This header contains the public interface for the linked list.
+ * The actual implementation is private and hidden from the client.
+ * The interface is as generic as possible in order to be very flexible.
+ * This code is heavily inspired on material obtained from the Algorithms and Data Structures
+ * course at IST Lisboa.
+ * @author Francisco Pombal
+ * @bug No known bugs
+ * @see https://www.tecnico.ulisboa.pt
+ */
+
+#ifndef LINKED_LIST_H
+#define LINKED_LIST_H
+
+/**
+ * Include the defs file
+ * @see The documentation for the defs.h header
+ */
 #ifndef DEFS_H
 #include "./defs.h"
 #endif
 
+
+/**
+ * @brief Struct for the singly-linked list.
+ *
+ * The implementation is hidden from the client,
+ */
 typedef struct SinglyLinkedList_ SinglyLinkedList;
+
+/**
+ * @brief Struct for the doubly-linked list.
+ *
+ * The implementation is hidden from the client,
+ */
 typedef struct DoublyLinkedList_ DoublyLinkedList;
 
-SinglyLinkedList* SinglyLinkedList_newNode(Item);
-void SinglyLinkedList_freeNode(SinglyLinkedList*, void(*)(Item));
-void SinglyLinkedList_freeList(SinglyLinkedList*, void (*)(Item));
-void SinglyLinkedList_insertNext(SinglyLinkedList*, SinglyLinkedList*);
-void SinglyLinkedList_insertAtEnd(SinglyLinkedList*, SinglyLinkedList*);
-void SinglyLinkedList_deleteNextNode(SinglyLinkedList*, void(*)(Item));
+
+/** @brief Creates a new node for a singly-linked list (with a payload Item) and returns a pointer to it
+ *
+ * Heap memory is alocated for a @SinglyLinkedList object.
+ * The next field is initialized to NULL.
+ * The item field will point to the specified payload
+ *
+ * @param item The payload to be held by the newly created node.
+ * @return new_node A pointer to the newly alocated node
+ */
+SinglyLinkedList* SinglyLinkedList_newNode(Item item);
+
+/** @brief Frees the memory allocated to a node and corresponding payload.
+ *
+ * This function effectivelly deletes the node/link
+ * First, the memory associated with the payload is freed by calling the appropriated function
+ * specified by the function pointer Item_Free.
+ * Finally, we simply free the node.
+ *
+ * @param node A pointer to the node to be freed.
+ * @param Item_Free Function pointer to a function that frees the payload of the node.
+ * @return void
+ */
+void SinglyLinkedList_freeNode(SinglyLinkedList* node, void(*Item_Free)(Item));
+
+/** @brief Frees the memory allocated to an entire singly-linked list.
+ *
+ * This function traverses the whole list, starting from the first node;
+ * For each node, the function SinglyLinkedList_freeNode is called.
+ *
+ * @param head A pointer to the first node of the singly linked list.
+ * @param Item_Free Function pointer to a function that frees the payload of the nodes.
+ * @return void
+ */
+void SinglyLinkedList_freeList(SinglyLinkedList* head, void (*Item_Free)(Item));
+
+/** @brief Inserts a node between node2 and the node node2 is pointing to
+ *
+ * To explain the behaviour of this function, suppose we have pointers to three nodes, head, node1 and node2, where head is a pointer to the first node of the list.
+ * After calling SinglyLinkedList_insertNext(node1, head), we will have:
+ * head ---> node1
+ * After calling SinglyLinkedList_insertNext(node2, head), we will have:
+ * head ---> node2 ---> node1
+ *
+ * @param node1 A pointer to the node to be inserted
+ * @param node2 A pointer to the node node1 will be inserted after
+ * @return void
+ */
+void SinglyLinkedList_insertNext(SinglyLinkedList* node1, SinglyLinkedList* node2);
+
+/** @brief Inserts a node at the end of a singly-linked list.
+ *
+ * To explain the behaviour of this function, suppose we have pointers to three nodes, head, node1 and node2, where head is a pointer to the first node of the singly-linked list.
+ * After calling SinglyLinkedList_insertAtEnd(head, node1), we will have:
+ * head ---> node1
+ * After calling SinglyLinkedList_insertAtEnd(head, node2), we will have:
+ * head ---> node1 ---> node2
+ *
+ * @param head A pointer to the first node of a singly-linked list
+ * @param node A pointer to the node to be inserted at the end of a singly-linked list
+ * @return void
+ */
+void SinglyLinkedList_insertAtEnd(SinglyLinkedList* head, SinglyLinkedList* node);
+
+/** @brief Deletes the node after the one this function is called on
+ *
+ * To explain the behaviour of this function, suppose we have pointers to three nodes, head, node1 and node2, where head is a pointer to the first node of the singly-linked list.
+ * Suppose these nodes are arranged in the following way:
+ * head ---> node1 ---> node2
+ * After calling this function on node1, we will have:
+ * head ---> node1
+ *
+ * @param node A pointer to the node before the one we want to free.
+ * @param Item_Free Function pointer to a function that frees the payload of the node.
+ * @return void
+ */
+void SinglyLinkedList_deleteNextNode(SinglyLinkedList* node, void(*Item_Free)(Item));
+
+
 SinglyLinkedList* SinglyLinkedList_getNextNode(SinglyLinkedList*); /* acessor*/
 Item SinglyLinkedList_getItem(SinglyLinkedList*); /* acessor */
 void SinglyLinkedList_setItem(SinglyLinkedList*, Item);
@@ -27,3 +135,5 @@ DoublyLinkedList* DoublyLinkedList_getNextNode(DoublyLinkedList*); /* acessor*/
 Item DoublyLinkedList_getItem(DoublyLinkedList*); /* acessor */
 void DoublyLinkedList_printListItems(DoublyLinkedList*, void(*)(Item));
 DoublyLinkedList* DoublyLinkedList_reverseList(DoublyLinkedList*);
+
+#endif
