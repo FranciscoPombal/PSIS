5144a633c48ae55680265bf70fb7aaac7104b0f0
FranciscoPombal <pombal.francisco@gmail.com>
2017-05-01 14:01:43 +0100

    Implement client menu skeleton, fix error condition, impl. signal handle

--- project_main/src/client.c
+++ project_main/src/client.c
@@ -1,11 +1,32 @@
 #include "../include/client.h"
 
+static volatile bool keepRunning = true;
+
+void sigIntHandler(int);
+
+void sigIntHandler(int sig)
+{
+    fprintf(stdout, "I think I caught SIGINT: %d; exiting\n", sig);
+    keepRunning = false;
+
+    return;
+}
+
 int main(void)
 {
     char* gateway_ipv4;
     int gateway_port;
     int choice = 0;
     int socket_stream_fd = 0;
+
+    // signal realated variables
+    struct sigaction sigint_action;
+
+        /* setup sigIntHandler as the handler function for SIGINT */
+        sigint_action.sa_handler = sigIntHandler;
+        sigemptyset(&sigint_action.sa_mask);
+        sigint_action.sa_flags = 0;
+        sigaction(SIGINT, &sigint_action, NULL);
 
         gateway_ipv4 = (char*)malloc(IPV4_STRING_SIZE * sizeof(char));
 
@@ -15,36 +36,49 @@ int main(void)
         fprintf(stdout, "\nGateway IP address: %s\nGateway port: %d\n", gateway_ipv4, gateway_port);
 
         socket_stream_fd = gallery_connect(gateway_ipv4, gateway_port);
-        if(socket_stream_fd == 0){
-            fprintf(stderr, "Unable to connect to gallery\n");
+        if(socket_stream_fd == -1){
+            fprintf(stderr, "Unable to connect to gallery, exiting. Try again later.\n");
+            exit(EXIT_FAILURE);
+        }else if (socket_stream_fd == 0){
+            fprintf(stderr, "Gateway says no peers are available. Try again later.\n");
             exit(EXIT_FAILURE);
         }
 
-        // TODO: rest of client
+        fprintf(stdout, "Client has contated the gateway and is connected to a peer.\n");
+
+// TODO: rest of client
+
+        while(true == keepRunning){
+            //Print client options
+            choice = showMenu();
+
+            //Execute option
+            switch(choice){
+                case 1:
+                    fprintf(stdout, "You chose to add a photo to the gallery\n");
+                    break;
+                case 2:
+                    fprintf(stdout, "You chose to add a keyword to a photo in the gallery\n");
+                    break;
+                case 3:
+                    fprintf(stdout, "You chose to search for a photo in the gallery\n");
+                    break;
+                case 4:
+                    fprintf(stdout, "You chose to delete a photo from the gallery\n");
+                    break;
+                case 5:
+                    fprintf(stdout, "You chose to get the name of a photo in the gallery\n");
+                    break;
+                case 6:
+                    fprintf(stdout, "You chose to  get (download) a photo from the gallery\n");
+                    break;
+                default:
+                    fprintf(stdout, "Invalid option number. Try again\n");
+                    break;
+            }
+        }
 
         free(gateway_ipv4);
-
-    // TODO: remove early exit (here for testing purposes only)
-    exit(EXIT_SUCCESS);
-
-        //Print client options
-        choice = showMenu();
-
-        //Execute option
-        switch (choice) {
-            case 1:
-                break;
-            case 2:
-                break;
-            case 3:
-                break;
-            case 4:
-                break;
-            case 5:
-                break;
-            default:
-                break;
-        }
 
     exit(EXIT_SUCCESS);
 }
