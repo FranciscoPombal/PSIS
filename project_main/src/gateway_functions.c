9fe61941e30a25a8db7cbe90a6c940a2ae177972
FranciscoPombal <pombal.francisco@gmail.com>
2017-05-04 00:03:03 +0100

    same

--- project_main/src/gateway_functions.c
+++ project_main/src/gateway_functions.c
@@ -1,7 +1,26 @@
-#include "../include/messages.h"
+#include "../include/gateway.h"
 
-int getGatewayPort(void);
+void sigIntHandler(int sig)
+{
+    fprintf(stdout, "Caught signal %d\n", sig);
+    keepRunning = 0;
 
+    return;
+}
+
+void setupInterrupt(void)
+{
+    // signal realated variables
+    struct sigaction sigint_action;
+
+        /* setup sigIntHandler as the handler function for SIGINT */
+        sigint_action.sa_handler = sigIntHandler;
+        sigemptyset(&sigint_action.sa_mask);
+        sigint_action.sa_flags = 0;
+        sigaction(SIGINT, &sigint_action, NULL);
+
+    return;
+}
 
 int getGatewayPort(void)
 {
