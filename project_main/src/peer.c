4fcdda418cc5b90660e0e58ecc87fe152135d087
FranciscoPombal <pombal.francisco@gmail.com>
2017-05-08 11:43:16 +0100

    Continuation of peer multithreading

--- project_main/src/peer.c
+++ project_main/src/peer.c
@@ -5,8 +5,6 @@ int main(void)
     // generic/program-specific variables
     int i = 0;
     Message message;
-    char* story = NULL;
-    long int story_len = 0;
 
     // socket/ipc related variables
     int socket_stream_fd = 0;
@@ -14,6 +12,11 @@ int main(void)
     int conn_sock_fd = 0;
     long int ret_val_send = 0;
     long int ret_val_recv = 0;
+
+    //thread stuff
+    pthread_t thread_master_client_accept_id = 0;
+    pthread_t thread_pinger_id = 0;
+    int ret_val_phtread_create = 0;
 
         // Setup SIGINT handler
         setupInterrupt();
@@ -25,43 +28,19 @@ int main(void)
         // dgram socket (for sending and receiving)
         socket_dgram_fd = gatewayConnect(socket_stream_fd);
 
-        fprintf(stdout, "Peer address sent to gateway\n");
+        fprintf(stdout, "Peer socket stream address sent to gateway via the dgram socket.\n");
+
+        // TODO: setup the Pinger thread
+        ret_val_phtread_create = pthread_create(&thread_pinger_id, NULL, &threadPinger, &socket_dgram_fd)
+
+        // TODO: setup the accept thread
 
         // TODO: the rest of the peer
         while(true == keepRunning){
 
             conn_sock_fd = accept(socket_stream_fd, NULL, NULL);
 
-            // receive message from client
-            ret_val_recv = recv(conn_sock_fd, message.buffer, MESSAGE_LEN, NO_FLAGS);
-
-            if(strlen(message.buffer) != 0){
-                /* process message */
-                fprintf(stdout, "Received message: %s\n", message.buffer);
-                story = realloc(story, strlen(story) + strlen(message.buffer) + 1);
-                story = strcat(story, message.buffer);
-                /* reset buffer receive buffer */
-                for(i = 0; i < MESSAGE_LEN; i++){
-                    message.buffer[i] = '\0';
-                }
-            }
-
-            story_len = strlen(story);
-
-            // send story back to client
-            if(ret_val_recv != -1){
-                ret_val_send = send(conn_sock_fd, &story_len, sizeof(long int), NO_FLAGS);
-                if(ret_val_send == -1){  // check for error
-                    fprintf(stderr, "Error sending story length.\n");
-                    exit(EXIT_FAILURE);
-                }
-
-                ret_val_send = send(conn_sock_fd, story, strlen(story), NO_FLAGS);
-                if(ret_val_send == -1){  // check for error
-                    fprintf(stderr, "Error sending story.\n");
-                    exit(EXIT_FAILURE);
-                }
-            }
+            // TODO: call thread to handle each client. is thread detached
 
             // close connection with current client
             close(conn_sock_fd);
@@ -71,13 +50,6 @@ int main(void)
         close(socket_stream_fd);
         close(socket_dgram_fd);
         fprintf(stdout, "Caught SIGINT, exiting cleanly\n");
-
-        /* print the final story */
-        if(strlen(story) != 0){
-            fprintf(stdout, "*** STORY:\n%s\n", story);
-        }
-
-    free(story);
 
     exit(EXIT_SUCCESS);
 }
